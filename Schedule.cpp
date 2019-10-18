1744f6a3a16c60438ffb80eefa686debf77ac96f
VictoriaTodd <a1719597@student.adelaide.edu.au>
2019-10-18 11:14:13 +1030

    Fixed decs, write file

--- Schedule.cpp
+++ Schedule.cpp
@@ -1,5 +1,5 @@
-#include "Lecturers.h"
-#include "Classes.h"
+#include "Declarations.h"
+#include <fstream> 
 
 
 int main(){
@@ -10,6 +10,13 @@ int main(){
 	//create optimal timetable - algorithm?
 
 	//output result to fnl_soln.sch
+
+	std::string tempresult = "my text here! \nand a new line"; //for testing purposes only
+	std::ofstream outfile ("fnl_soln.sch");
+
+	outfile <<  tempresult << std::endl;
+
+	outfile.close();
 
 	return 0;
 }
